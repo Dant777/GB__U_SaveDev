e8495be1b30def59bf3d3e640d5b60cd189bf740
Dant777 <47300885+Dant777@users.noreply.github.com>
2022-02-26 11:12:47 +0300

    Подготовка к 1 миграции

---
+++ AbstractionLayer/Entities/BankCard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractionLayer.Entities
+{
+    public class BankCard
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public int CardNumber { get; set; }
+    }
+}
--- DataLayer/AppDataContext.cs
+++ DataLayer/AppDataContext.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AbstractionLayer.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataLayer
 {
     public class AppDataContext:DbContext
     {
-
+        public AppDataContext(DbContextOptions<AppDataContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<BankCard> BankCards { get; set; }
     }
+
+    
 }
