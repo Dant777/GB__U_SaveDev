09076fd3b8cd30d14708b904436aae757e705fcb
Dant777 <47300885+Dant777@users.noreply.github.com>
2022-03-01 16:15:14 +0300

    Создание контроллера регистрации и логина

---
+++ GB__U_SaveDev/Controllers/AuthManagerController.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Core.Entities.Configuration;
+using Domain.Core.RequestEntities;
+using Domain.Core.ResponseEntities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace GB__U_SaveDev.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthManagerController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly JwtConfig _jwtConfig;
+
+        public AuthManagerController(
+            UserManager<IdentityUser> userManager, 
+            IOptionsMonitor<JwtConfig>  optionsMonitor)
+        {
+            _userManager = userManager;
+            _jwtConfig = optionsMonitor.CurrentValue;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody]UserRegistrationRequest  user)
+        {
+            if (ModelState.IsValid)
+            {
+                    
+
+            }
+
+            return BadRequest(new RegistrationResponse()
+            {
+                Erorrs = new List<string>()
+                {
+                    "Error registration"
+
+                }
+
+            });
+        }
+    }
+}
